54e3643fa14ea73d6d0053b319640cf1c22d6ffb
radioActive DROID <me@godwin.dev>
2023-04-21 10:22:20 +0100

    fix test and tweak global.json

--- tests/Service/Services/GenericRepositoryTests.cs
+++ tests/Service/Services/GenericRepositoryTests.cs
@@ -1,5 +1,5 @@
 // Copyright 2022 - 2023 Godwin peter .O (me@godwin.dev)
-// 
+//
 // Licensed under the MIT License;
 // you may not use this file except in compliance with the License.
 // Unless required by applicable law or agreed to in writing, software
@@ -13,70 +13,69 @@ using ParkingSpace.Common.Entity;
 using ParkingSpace.Features.Customer.Entities;
 using ParkingSpace.Filters;
 
-namespace ParkingSpace.Tests.Services;
+namespace ParkingSpace.Tests.Services {
+    [TestCaseOrderer(
+        ordererTypeName: "ParkingSpace.Tests.AlphabeticalOrderer",
+        ordererAssemblyName: "ParkingSpace.Tests")]
+    [Collection("api-context")]
+    public class GenericRepositoryTests {
+        private readonly AsyncServiceScope _scope;
+        private readonly IRepository<Customer>? _service;
 
-[TestCaseOrderer(
-    ordererTypeName: "ParkingSpace.Tests.AlphabeticalOrderer",
-    ordererAssemblyName: "ParkingSpace.Tests")]
-[Collection("api-context")]
-public class GenericRepositoryTests {
-    private readonly AsyncServiceScope _scope;
-    private readonly IRepository<Customer>? _service;
+        public GenericRepositoryTests(ServiceFactory factory) {
+            _scope = factory.Services.CreateAsyncScope();
+            _service = _scope.ServiceProvider.GetService<IRepository<Customer>>();
+        }
 
-    public GenericRepositoryTests(ServiceFactory factory) {
-        _scope = factory.Services.CreateAsyncScope();
-        _service = _scope.ServiceProvider.GetService<IRepository<Customer>>();
-    }
+        private static readonly Customer SingleSample = new() {
+            Id = Guid.NewGuid(),
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@mail.xx",
+            Phone = "+1303930333"
+        };
 
-    private static readonly Customer SingleSample = new Customer {
-        Id = Guid.NewGuid(),
-        FirstName = "John",
-        LastName = "Doe",
-        Email = "john.doe@mail.xx",
-        Phone = "+1303930333"
-    };
+        [Fact]
+        public async Task GenericService0AddAsyncTest() {
+            Customer result = await _service!.AddAsync(SingleSample);
 
-    [Fact]
-    public async Task GenericService0AddAsyncTest() {
-        var result = await _service!.AddAsync(SingleSample);
+            Assert.NotNull(result);
+            Assert.Equal(SingleSample.Id, result.Id);
+        }
 
-        Assert.NotNull(result);
-        Assert.Equal(SingleSample.Id, result.Id);
-    }
+        [Fact]
+        public async Task GenericService1GetByIdAsyncTest() {
+            Customer? result = await _service!.GetByIdAsync(SingleSample.Id);
 
-    [Fact]
-    public async Task GenericService1GetByIdAsyncTest() {
-        var result = await _service!.GetByIdAsync(SingleSample.Id);
+            Assert.NotNull(result);
+        }
 
-        Assert.NotNull(result);
-    }
+        [Fact]
+        public async Task GenericService2GetAllTest() {
+            IEnumerable<Customer> result = await _service!.GetAllAsync(new PageFilter());
 
-    [Fact]
-    public async Task GenericService2GetAllTest() {
-        var result = await _service!.GetAllAsync(new PageFilter());
+            _ = Assert.IsAssignableFrom<IEnumerable<Customer>>(result);
+            Assert.NotEmpty(result);
+        }
 
-        Assert.IsAssignableFrom<IEnumerable<Customer>>(result);
-        Assert.NotEmpty(result);
-    }
+        [Fact]
+        public async Task GenericService3UpdateAsyncTest() {
+            SingleSample.FirstName = "Jane";
+            await _service!.UpdateAsync(SingleSample);
+            Customer? result = await _service!.GetByIdAsync(SingleSample.Id);
 
-    [Fact]
-    public async Task GenericService3UpdateAsyncTest() {
-        SingleSample.FirstName = "Jane";
-        await _service!.UpdateAsync(SingleSample);
-        var result = await _service!.GetByIdAsync(SingleSample.Id);
+            Assert.Equal(result!.FirstName, SingleSample.FirstName);
+        }
 
-        Assert.Equal(result!.FirstName, SingleSample.FirstName);
-    }
+        [Fact]
+        public async Task GenericService4ArchiveAsyncTest() {
+            await _service!.ArchiveAsync(SingleSample);
+            Customer? result = await _service!.GetByIdAsync(SingleSample.Id);
 
-    [Fact]
-    public async Task GenericService4ArchiveAsyncTest() {
-        await _service!.ArchiveAsync(SingleSample);
-        var result = await _service!.GetByIdAsync(SingleSample.Id);
+            Assert.Null(result);
+        }
 
-        Assert.Null(result);
-    }
-
-    private static readonly List<Customer> MultipleSample = new List<Customer> {
+        private static readonly List<Customer> MultipleSample = new() {
         new Customer {
             Id = Guid.NewGuid(),
             FirstName = "John",
@@ -93,30 +92,31 @@ public class GenericRepositoryTests {
         }
     };
 
-    [Fact]
-    public async Task GenericService5AddRangeAsyncTest() {
-        var result = await _service!.AddRangeAsync(MultipleSample);
+        [Fact]
+        public async Task GenericService5AddRangeAsyncTest() {
+            IEnumerable<Customer> result = await _service!.AddRangeAsync(MultipleSample);
 
-        Assert.NotEmpty(result);
-    }
+            Assert.NotEmpty(result);
+        }
 
-    [Fact]
-    public async Task GenericService6UpdateRangeAsyncTest() {
-        MultipleSample[0].FirstName = "JohnX";
-        await _service!.UpdateRangeAsync(MultipleSample);
-        var result = await _service!.GetAllAsync(new PageFilter());
+        [Fact]
+        public async Task GenericService6UpdateRangeAsyncTest() {
+            MultipleSample[0].FirstName = "JohnX";
+            await _service!.UpdateRangeAsync(MultipleSample);
+            IEnumerable<Customer> result = await _service!.GetAllAsync(new PageFilter());
 
-        IEnumerable<Customer> collection = result.ToList();
-        Assert.NotEmpty(collection);
-        var data = collection.Select(x => x.FirstName).FirstOrDefault(x => x == "JohnX");
-        Assert.Equal(MultipleSample[0].FirstName, data);
-    }
+            IEnumerable<Customer> collection = result.ToList();
+            Assert.NotEmpty(collection);
+            string? data = collection.Select(x => x.FirstName).FirstOrDefault(x => x == "JohnX");
+            Assert.Equal(MultipleSample[0].FirstName, data);
+        }
 
-    [Fact]
-    public async Task GenericService7ArchiveRangeAsyncTest() {
-        await _service!.ArchiveRangeAsync(MultipleSample);
-        var result = await _service!.GetAllAsync(new PageFilter());
+        [Fact]
+        public async Task GenericService7ArchiveRangeAsyncTest() {
+            await _service!.ArchiveRangeAsync(MultipleSample);
+            IEnumerable<Customer> result = await _service!.GetAllAsync(new PageFilter());
 
-        Assert.Empty(result);
+            Assert.Empty(result);
+        }
     }
 }
