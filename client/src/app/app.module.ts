cfd0c25e1ca76e20b2913ef0e2d51e5fcbe00b46
radioActive DROID <me@godwin.dev>
2023-11-16 22:51:09 +0100

    remove sample pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,15 +6,6 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SpaceModule } from './space/space.module';
-import { CustomerModule } from './customer/customer.module';
-import { IncidentModule } from './incident/incident.module';
-import { PriceModule } from './price/price.module';
-import { StaffModule } from './staff/staff.module';
-import { TicketModule } from './ticket/ticket.module';
-import { VehicleModule } from './vehicle/vehicle.module';
-import { SpotModule } from './spot/spot.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,16 +13,7 @@ import { SpotModule } from './spot/spot.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    CustomerModule,
-    IncidentModule,
-    PriceModule,
-    SpaceModule,
-    SpotModule,
-    StaffModule,
-    TicketModule,
-    VehicleModule,
     HttpClientModule,
-    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
