0aaa7a127a19f7357ed60e1fc25ff01441f1dd4d
radioActive DROID <me@godwin.dev>
2023-11-17 00:02:49 +0100

    update to angular version 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FormsModule,
+    HttpClientModule,
+    MatToolbarModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Parking Space';
