f1ee6fcb35a34855a28ed2dee0c086381f2dcbee
Godwin peter. O <me@godwin.dev>
2025-07-09 13:22:21 +0100

    chore: added material-ui to project

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule, MatButton],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Test - {{title}}!</h1>
+    <button matButton="elevated">Check Component</button>
 
     <router-outlet />
   `,
   styles: [],
 })
 export class App {
-  protected title = 'client';
+  protected title = 'Parking Space';
 }
